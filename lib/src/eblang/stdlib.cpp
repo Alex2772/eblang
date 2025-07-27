b3aa1c65eddb84d98ecea2082deb0d854d655526
alex2772 <alex2772sc@gmail.com>
2025-07-27 19:25:58 +0300

    input

--- lib/src/eblang/stdlib.cpp
+++ lib/src/eblang/stdlib.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include "stdlib.h"
+#include "util.h"
 
 using namespace eblang;
 
@@ -15,6 +16,33 @@ eblang::Value println(eblang::Context& state, const std::vector<eblang::Value>& 
     std::cout << std::endl;
     return std::monostate();
 }
+
+eblang::Value input(eblang::Context& state, const std::vector<eblang::Value>& args) {
+    std::string str;
+    std::cin >> str;
+    return str;
+}
+
+eblang::Value toint(eblang::Context& state, const std::vector<eblang::Value>& args) {
+    if (args.size() != 1) {
+        throw std::runtime_error("toint expects 1 argument");
+    }
+    return std::visit(
+        eblang::match {
+          [](auto&& v) -> int { return int(v); },
+          [](const std::string& s) -> int {
+              return std::stoi(s);
+          },
+          [](std::monostate) -> int { throw std::runtime_error("toint: can't convert void to int"); },
+        },
+        args[0]);
+}
 }   // namespace
 
-void eblang::stdlib::init(eblang::State& state) { state.context().functions["println"] = { println }; }
+void eblang::stdlib::init(eblang::State& state) {
+#define DEFINE(name) state.context().functions[#name] = { name }
+
+    DEFINE(println);
+    DEFINE(input);
+    DEFINE(toint);
+}
